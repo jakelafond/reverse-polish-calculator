b276c5220bc765a4a8cf553c6535623906f377ee
Jake LaFond <jakelafond@gmail.com>
2017-08-17 15:34:08 -0400

    DONE

--- Program.cs
+++ Program.cs
@@ -13,13 +13,28 @@ namespace Daily_Project_Reverse_Polish_Calculator
             var actualResult = rpc.Calculate(rpnExpression);
             checkResult(rpnExpression, expectedResult, actualResult);
 
+            rpnExpression = "12,2.7,-";
+            expectedResult = 9.3;
+            actualResult = rpc.Calculate(rpnExpression);
+            checkResult(rpnExpression, expectedResult, actualResult);
+
+            rpnExpression = "3.5,52,*";
+            expectedResult = 182;
+            actualResult = rpc.Calculate(rpnExpression);
+            checkResult(rpnExpression, expectedResult, actualResult);
+
+            rpnExpression = "4.3,2,/";
+            expectedResult = 2.15;
+            actualResult = rpc.Calculate(rpnExpression);
+            checkResult(rpnExpression, expectedResult, actualResult);
+
             // Write tests for the other operators (-, *, /)
 
             // Uncomment this test which has many operators
-            //        rpnExpression = "5,1,2,+,4,*,+,3,-";
-            //        expectedResult = 14.0;
-            //        actualResult = rpn.calculate(rpnExpression);
-            //        checkResult(rpnExpression, expectedResult, actualResult);
+                   rpnExpression = "5,1,2,+,4,*,+,3,-";
+                   expectedResult = 14.0;
+                   actualResult = rpc.Calculate(rpnExpression);
+                   checkResult(rpnExpression, expectedResult, actualResult);
         }
 
         private static void checkResult(String expression, double expected, double actual)
